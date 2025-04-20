bf584d664190e9d918abca6d9a6bacd6b25fcdb5
fuji-byte <kandaiunivsenyou@gmail.com>
2025-04-20 23:18:31 +0900

    layout.tsxのリンク編集

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,17 +8,17 @@ export const metadata: Metadata = {
   title: "Kandai Stamp v1.0.0",
   description: "関大文化フェスティバルスタンプ企画",
   alternates: {
-    canonical: "https://stamp-rally-azure.vercel.app/",
+    canonical: "https://stamprally-two.vercel.app/",
   },
-  metadataBase: new URL("https://stamp-rally-azure.vercel.app/"),
+  metadataBase: new URL("https://stamprally-two.vercel.app/"),
   openGraph: {
     title: "関大文化フェス",
     siteName: "関大文化フェス",
     type: "website",
-    url:"https://stamp-rally-azure.vercel.app",
+    url:"https://stamprally-two.vercel.app",
     images: [
       {
-        url: "https://stamp-rally-azure.vercel.app/og-image.png",
+        url: "https://stamprally-two.vercel.app/og-image.png",
         width: 1200,
         height: 1200,
         alt: "関大文化フェス",
