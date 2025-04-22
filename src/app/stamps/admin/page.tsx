d187e01875e5256d73ad1bb90d500d34a82a33ad
yoshi0319 <yoshi3YH@icloud.com>
2025-04-23 03:49:38 +0900

    スタンプ全所持者絞り込みを実装

--- src/app/stamps/admin/page.tsx
+++ src/app/stamps/admin/page.tsx
@@ -8,9 +8,14 @@ export default function AdminStampPage() {
   type StampWithEmail = {
     userId: number
     email: string
-    stamps: Stamps
+    stamp1: boolean | null
+    stamp2: boolean | null
+    stamp3: boolean | null
+    stamp4: boolean | null
+    stamp5: boolean | null
   }
   const [data, setData] = useState<StampWithEmail[]>([])
+  const [originalData, setOriginalData] = useState<StampWithEmail[]>([])
   const [loading, setLoading] = useState(true)
   const [ errorMessage, setErrorMessage] = useState<string>('')
   const router = useRouter()
@@ -29,9 +34,24 @@ export default function AdminStampPage() {
           }, 3000);
         }
         setData(res.data)
+        setOriginalData(res.data)
         setLoading(false)
       })
   }, [])
+
+  const handleReset = () => {
+    setData(originalData)
+  }
+
+  const handleSort = () => {
+    const sortedData = data.filter(user => {
+      return [1, 2, 3, 4, 5].every(i => 
+        user[`stamp${i}` as keyof StampWithEmail] === true
+      )
+    })
+    setData(sortedData)
+  }
+
 
   if (loading) return <p className="p-4">読み込み中...</p>
 
@@ -42,6 +62,14 @@ export default function AdminStampPage() {
   ) : (
         <div className="p-6">
         <h1 className="text-2xl font-bold mb-4">スタンプ管理画面</h1>
+        <div className="flex gap-5 pb-5">
+          <button 
+          onClick={handleReset}
+          className="px-5 py-3 bg-white text-black rounded hover:bg-gray-400">リセット</button>
+          <button
+          onClick={handleSort}
+          className="px-5 py-3 bg-blue-500 text-white rounded hover:bg-blue-600">ソート</button>
+        </div>
         <table className="table-auto border w-full text-sm">
           <thead>
             <tr>
